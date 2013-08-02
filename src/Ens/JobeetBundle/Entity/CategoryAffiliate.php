fd0bccbfc6f6004c6f9bb083eaaad7b6053c66ff
Shane Rogers <swrogers@gmail.com>
2013-08-02 16:28:46 -0500

    Added entities

---
+++ src/Ens/JobeetBundle/Entity/CategoryAffiliate.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Ens\JobeetBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * CategoryAffiliate
+ *
+ * @ORM\Table()
+ * @ORM\Entity(repositoryClass="Ens\JobeetBundle\Entity\CategoryAffiliateRepository")
+ */
+class CategoryAffiliate
+{
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+
+    /**
+     * Get id
+     *
+     * @return integer 
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+}
