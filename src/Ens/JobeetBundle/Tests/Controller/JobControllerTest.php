6f2d67a8a06051d44f2012a0f3838e8126e51328
Shane Rogers <swrogers@gmail.com>
2013-08-14 09:27:27 -0500

    More functional tests

--- src/Ens/JobeetBundle/Tests/Controller/JobControllerTest.php
+++ src/Ens/JobeetBundle/Tests/Controller/JobControllerTest.php
@@ -32,14 +32,30 @@ class JobControllerTest extends WebTestCase
         $this->assertTrue($crawler->filter('.category_programming .more_jobs')->count() == 1);
 
         // Make sure jobs are sorted by date
+        $this->assertTrue($crawler->filter('.category_programming tr')->first()->filter(sprintf('a[href*="/%d/"]', $this->getMostRecentProgrammingJob()->getId()))->count() == 1);
+
+        // Make sure each job on the homepage is clickable
+        $job = $this->getMostRecentProgrammingJob();
+        $link = $crawler->selectLink('Web Developer')->first()->link();
+        $crawler = $client->click($link);
+        $this->assertEquals('Ens\JobeetBundle\Controller\JobController::showAction', $client->getRequest()->attributes->get('_controller'));
+        $this->assertEquals($job->getCompanySlug(), $client->getRequest()->attributes->get('company'));
+        $this->assertEquals($job->getLocationSlug(), $client->getRequest()->attributes->get('location'));
+        $this->assertEquals($job->getPositionSlug(), $client->getRequest()->attributes->get('position'));
+        $this->assertEquals($job->getId(), $client->getRequest()->attributes->get('id'));
+    }
+
+    public function getMostRecentProgrammingJob()
+    {
+        $kernel = static::createKernel();
+        $kernel->boot();
         $em = $kernel->getContainer()->get('doctrine.orm.entity_manager');
-        
+
         $query = $em->createQuery('SELECT j from EnsJobeetBundle:Job j LEFT JOIN j.category c WHERE c.slug = :slug AND j.expires_at > :date ORDER BY j.createdAt DESC');
         $query->setParameter('slug', 'programming');
         $query->setParameter('date', date('Y-m-d H:i:s', time()));
         $query->setMaxResults(1);
-        $job = $query->getSingleResult();
-
-        $this->assertTrue($crawler->filter('.category_programming tr')->first()->filter(sprintf('a[href*="/%d/"]', $job->getId()))->count() == 1);
+        
+        return $query->getSingleResult();
     }
 }
