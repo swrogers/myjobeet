4c228a703a77b60c63719afb5dbd75f48926754b
Shane Rogers <swrogers@gmail.com>
2013-08-22 14:54:18 -0500

    Added User and basic security. Added logout and override user_block

--- src/Ens/JobeetBundle/Controller/DefaultController.php
+++ src/Ens/JobeetBundle/Controller/DefaultController.php
@@ -3,11 +3,39 @@
 namespace Ens\JobeetBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\Security\Core\SecurityContext;
+use Symfony\Component\HttpFoundation\Request;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+
 
 class DefaultController extends Controller
 {
     public function indexAction($name)
     {
         return $this->render('EnsJobeetBundle:Default:index.html.twig', array('name' => $name));
+    }
+
+    /**
+     * @Route("/login", name="login")
+     * @Template()
+     */
+    public function loginAction(Request $request)
+    {
+        $session = $request->getSession();
+
+        if($request->attributes->has(SecurityContext::AUTHENTICATION_ERROR))
+        {
+            $error = $request->attributes->get(SecurityContext::AUTHENTICATION_ERROR);
+        } else {
+            $error = $session->get(SecurityContext::AUTHENTICATION_ERROR);
+            $session->remove(SecurityContext::AUTHENTICATION_ERROR);
+        }
+
+        return array(
+            'last_username' => $session->get(SecurityContext::LAST_USERNAME),
+            'error' => $error,
+        );
     }
 }
---
+++ src/Ens/JobeetBundle/Entity/User.php
@@ -0,0 +1,131 @@
+<?php
+
+namespace Ens\JobeetBundle\Entity;
+
+use Symfony\Component\Security\Core\User\UserInterface;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * User
+ *
+ * @ORM\Table()
+ * @ORM\Entity(repositoryClass="Ens\JobeetBundle\Entity\UserRepository")
+ */
+class User implements UserInterface
+{
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="username", type="string", length=255)
+     */
+    private $username;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="password", type="string", length=255)
+     */
+    private $password;
+
+
+    /**
+     * Get id
+     *
+     * @return integer 
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set username
+     *
+     * @param string $username
+     * @return User
+     */
+    public function setUsername($username)
+    {
+        $this->username = $username;
+    
+        return $this;
+    }
+
+    /**
+     * Get username
+     *
+     * @return string 
+     */
+    public function getUsername()
+    {
+        return $this->username;
+    }
+
+    /**
+     * Set password
+     *
+     * @param string $password
+     * @return User
+     */
+    public function setPassword($password)
+    {
+        $this->password = $password;
+    
+        return $this;
+    }
+
+    /**
+     * Get password
+     *
+     * @return string 
+     */
+    public function getPassword()
+    {
+        return $this->password;
+    }
+
+    /**
+     * Get roles
+     * Required for UserInterface implementation
+     */
+    public function getRoles()
+    {
+        return array('ROLE_ADMIN');
+    }
+
+    /**
+     * Get salt
+     * Required for UserInterface implementation
+     */
+    public function getSalt()
+    {
+        return null;
+    }
+
+    /**
+     * Erase Credentials
+     * Required for UserInterface implementation
+     */
+    public function eraseCredentials()
+    {
+
+    }
+
+    /**
+     * Compare two usernames
+     * Required for UserInterface implementation
+     */
+    public function equals(UserInterface $user)
+    {
+        return $user->getUsername() == $this->getUsername();
+    }
+}
